a5a54b59d7bfdbca191a60c6c7b78187623ea825
ClauBloom <clauvenater_@outlook.com>
2025-09-22 01:13:46 +0800

    Fixed some bugs,Add some features about multicases

--- data-generator.h
+++ data-generator.h
@@ -1,6 +1,9 @@
 //
 // Created by clau on 25-9-18.
 //
+//
+// Created by clau on 25-9-17.
+//
 
 #ifndef DATA_GENERATOR_H
 #define DATA_GENERATOR_H
@@ -14,11 +17,11 @@
 #include <functional>
 #include "testlib.h"
 /*
- 使 *用流程
- 1. 使用数据生成器，并暂存至stringstream
- 2. 使用solver获取答案，并暂存至stringstream
- 3. 使用input前缀函数输出数据
- 4. 使用output前缀函数输出数据
+使用流程
+1. 使用数据生成器，并暂存至stringstream
+2. 使用solver获取答案，并暂存至stringstream
+3. 使用input前缀函数输出数据
+4. 使用output前缀函数输出数据
  */
 template<typename T>
 class gen {
@@ -31,17 +34,18 @@ private:
     T solver;
     std::stringstream inputs;//用于执行solver的流
     std::stringstream outputs;//用于存储solver的结果
+    std::vector<int> multiSizes;
 public:
     gen(std::string _name,int _num,bool isMulti,T& solve,int argc,char* argv[])
     : name(_name),num(_num),isMultiCase(isMulti),
-    in(_name + std::string("/") + std::string("input") + std::to_string(num) + std::string(".in")),
-    out(_name + std::string("/") + std::string("output") + std::to_string(num) + std::string(".out")),solver(solve) {
+    in(_name + std::string("/") + std::string("input") + std::to_string(num) + std::string(".in"),isMulti ? std::ios::app : std::ios::out),
+    out(_name + std::string("/") + std::string("output") + std::to_string(num) + std::string(".out"),isMulti ? std::ios::app : std::ios::out),
+    solver(solve) {
         std::filesystem::create_directories(_name);
-        if(isMultiCase) {
-            inputs<<1;
-        }
     }
+    std::vector<int> getMultiCaseArraySize(int n,int size);
     //输入测试点
+    void inputSpace();
     void inputArray(std::vector<int> a);
     void inputString(std::string);
     void inputInt(int n);
@@ -69,12 +73,13 @@ public:
     int getInt();
     int getIntPos();
     int getIntRange(int l,int r);
+    int getIntRangeNoStream(int l,int r);
     int getIntRangePos(int v);
     long long getLongLong();
     long long getLongLongRange(int v);
     //std答案
     void outputArray(std::vector<int> a);
-    void outputInt(int n);
+    void appendOutputInt(int n);
     void outputArrayWithoutn(std::vector<int> a);
     void setInputString(std::string s);
     void outputAll() ;
@@ -83,12 +88,12 @@ public:
     }
 };
 
-
 #endif //DATA_GENERATOR_H
-
 
 template<typename T>
 void gen<T>::solve() {
+    outputs.clear();
+    outputs.str("");
     std::streambuf* cinbuf = std::cin.rdbuf();
     std::streambuf* coutbuf = std::cout.rdbuf();
     std::cin.rdbuf(inputs.rdbuf());
@@ -124,6 +129,11 @@ int gen<T>::getIntRange(int l,int r) {
     return x;
 }
 template<typename T>
+int gen<T>::getIntRangeNoStream(int l,int r) {
+    int x = rnd.next(l,r);
+    return x;
+}
+template<typename T>
 int gen<T>::getIntRangePos(int v) {
     int x = rnd.next(1,v);
     inputs<<x<<' ';
@@ -135,7 +145,7 @@ std::string gen<T>::getStringLowerAlpha(int n) {
     for(int i = 0;i<n;i++) {
         res[i] = 'a' + rnd.next(0,25);
     }
-    inputs<<res<<std::endl;;
+    inputs<<res<<std::endl;
     return res;
 }
 template<typename T>
@@ -144,10 +154,7 @@ void gen<T>::setInputString(std::string s) {
 }
 template<typename T>
 void gen<T>::outputAll() {
-    std::string s;
-    while(getline(outputs,s)) {
-        out<<s;
-    }
+    out << outputs.rdbuf();
 }
 template<typename T>
 void gen<T>::inputString(std::string s) {
@@ -206,4 +213,55 @@ void gen<T>::inputArray64Withoutn(std::vector<long long> a) {
     if(a.size() > 0) {
         in<<a.back()<<std::endl;
     }
+}
+template<typename T>
+void gen<T>::inputArrayWithoutn(std::vector<int> a) {
+    for(int i = 0;i<a.size() - 1;i++) {
+        in<<a[i]<<' ';
+    }
+    if(a.size() > 0) {
+        in<<a.back()<<std::endl;
+    }
+}
+template<typename T>
+void gen<T>::inputSpace() {
+    in<<' ';
+}
+template<typename T>
+std::vector<int> gen<T>::getMultiCaseArraySize(int size,int value) {
+    if (size == 0) {
+        return {};
+    }
+    if (value < size) {
+        // 无法满足每个元素至少为1，返回空数组
+        return {};
+    }
+    int R = value - size;
+    if (R == 0) {
+        return std::vector<int>(size, 1);
+    }
+
+    // 生成 size-1 个随机点
+    std::vector<int> random_points;
+    for (int i = 0; i < size - 1; i++) {
+        random_points.push_back(rnd.next(0,R));
+    }
+
+    // 对随机点排序
+    std::sort(random_points.begin(), random_points.end());
+
+    std::vector<int> result;
+    int prev = 0;
+    for (int i = 0; i < size - 1; i++) {
+        int diff = random_points[i] - prev;
+        result.push_back(1 + diff);
+        prev = random_points[i];
+    }
+    result.push_back(1 + R - prev);
+
+    return result;
+}
+template<typename T>
+void gen<T>::appendOutputInt(int n) {
+    inputs<<n<<' ';
 }
